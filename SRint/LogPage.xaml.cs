bf40d3a6cf0286c78d7b4408114750eb904d1380
Maciej 'mac' Grzybek <mashester@gmail.com>
2014-01-14 16:21:20 +0100

    Removed unnecessary code from LogPage

--- SRint/LogPage.xaml.cs
+++ SRint/LogPage.xaml.cs
@@ -23,7 +23,6 @@ namespace SRint
         public LogPage(MainWindow parent)
         {
             InitializeComponent();
-            this.parent = parent;
             Logger.Instance.OnErrorOccured += OnErrorOccured;
             Logger.Instance.OnWarningOccured += OnWarningOccured;
             Logger.Instance.OnNoticeOccured += OnNoticeOccured;
@@ -60,39 +59,5 @@ namespace SRint
                     scrollViewer.ScrollToBottom();
                 });
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            DateTime dt = System.DateTime.Now;
-            protobuf.Message.State state = new protobuf.Message.State { state_id = 12 };
-            protobuf.Message.Variable variable = new protobuf.Message.Variable { name = "Test", value = 17 };
-            state.variables.Add(variable);
-            protobuf.Message msg = new protobuf.Message
-            {
-                type = protobuf.Message.MessageType.STATE,
-                state_content = state
-            };
-            string toSend = Encoding.ASCII.GetString(Serialization.MessageSerializer.Serialize(msg));
-            Logger.Instance.LogNotice("Sending: \"" + toSend + "\" of length: " + toSend.Length);
-            //parent.server.SendMessage(toSend);
-
-            //loggerTextBlock.Text += "\ncontext spawning";
-            //var context = new ZMQ.Context(1);
-            //loggerTextBlock.Text += "\nusing requester";
-            //using (ZMQ.Socket requester = context.Socket(SocketType.REQ))
-            //{
-            //    loggerTextBlock.Text += "\nconnecting to host";
-            //    requester.Connect("tcp://10.25.67.29:5555");
-            //    //requester.Connect("tcp://127.0.0.1:5555");
-
-            //    const string requestMessage = "Hello";
-            //    loggerTextBlock.Text += "Sending request...";
-            //    requester.Send(requestMessage, Encoding.ASCII);
-            //    string reply = requester.Recv(Encoding.ASCII, 2000);
-            //    loggerTextBlock.Text += "Received reply: {0}" + reply;
-            //}
-        }
-
-        MainWindow parent;
     }
 }
